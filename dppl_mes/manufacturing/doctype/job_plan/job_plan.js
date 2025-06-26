926acb665b07ad5d487e920f550684ec43355371
Navneet Jain <navneetjain89@gmail.com>
2025-06-26 18:14:07 +0530

    feat: Job Card Creation Process Automation

--- dppl_mes/manufacturing/doctype/job_plan/job_plan.js
+++ dppl_mes/manufacturing/doctype/job_plan/job_plan.js
@@ -1,8 +1,97 @@
 // Copyright (c) 2025, IndusWorks and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Job Plan", {
-// 	refresh(frm) {
+/*
+To Do:
+When a new form loads
+1. Check Organization Settings DocType and Look for default_factory field.
+2. If there is value in default_factory field then use the value and insert in factory field.
+3. Put today's date in date field in the form
+4. Using the factory fetch list of all machines where is_active is true and insert them in job_plan_details.
+5. if the user changes factory field option then empty the table.
 
-// 	},
-// });
+When the form is not new:
+1. If the frm.doc.status === 'Draft' show Button called 'Confirm'
+*/
+
+frappe.ui.form.on("Job Plan", {
+    refresh(frm) {
+        if (frm.is_new()) {
+            console.log("New form loaded");
+
+            // Set today's date
+            frm.set_value("date", frappe.datetime.get_today());
+
+            // Fetch and set default factory
+            frappe.db.get_single_value("Organization Settings", "default_factory")
+                .then(value => {
+                    if (value) {
+                        frm.set_value("factory", value);
+                    }
+                });
+        } else {
+            // If document is in Draft status, show Confirm button
+            if (frm.doc.status === "Draft") {
+                frm.add_custom_button(__('Confirm Job Plan'), () => {
+                    frappe.confirm(
+                        "On submission, Job Cards will be created.",
+                        () => {
+                            frappe.call({
+                                method: "dppl_mes.manufacturing.doctype.job_plan.job_plan.create_job_cards",
+                                args: {
+                                    docname: frm.doc.name
+                                },
+                                freeze: true,
+                                callback: function (r) {
+                                    if (!r.exc) {
+                                        frappe.msgprint("Job Cards Created Successfully.");
+                                        frm.reload_doc();
+                                    }
+                                }
+                            });
+                        },
+                        () => {
+                            // Cancelled
+                        }
+                    );
+                });
+                frm.add_custom_button(__('Cancel Job Plan'), () => {
+                    frm.set_value("status", "Cancelled");
+                    frm.save();
+                })
+            }
+        }
+    },
+
+    factory(frm) {
+        if (frm.doc.factory) {
+            // Clear existing table entries
+            frm.clear_table("job_plan_details");
+
+            // Fetch active machines linked to selected factory
+            frappe.db.get_list("Machine", {
+                filters: {
+                    factory: frm.doc.factory,
+                    is_active: 1
+                },
+                fields: ["machine_name"],
+                limit: 1000
+            }).then(machines => {
+                if (machines.length) {
+                    machines.forEach(machine => {
+                        let row = frm.add_child("job_plan_details", {
+                            machine_name: machine.machine_name
+                        });
+                    });
+                    frm.refresh_field("job_plan_details");
+                } else {
+                    frappe.msgprint("No active machines found for the selected factory.");
+                }
+            });
+        } else {
+            // If factory field is cleared manually, also clear table
+            frm.clear_table("job_plan_details");
+            frm.refresh_field("job_plan_details");
+        }
+    }
+});
--- dppl_mes/manufacturing/doctype/job_plan/job_plan.py
+++ dppl_mes/manufacturing/doctype/job_plan/job_plan.py
@@ -1,9 +1,62 @@
 # Copyright (c) 2025, IndusWorks and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class JobPlan(Document):
 	pass
+
+
+@frappe.whitelist()
+def create_job_cards(docname):
+    doc = frappe.get_doc("Job Plan", docname)
+
+    if doc.docstatus != 0:
+        frappe.throw(_("Only Draft Job Plans can be processed."))
+
+    created_count = 0
+
+    for row in doc.job_plan_details:
+        if row.no_job:
+            continue  # Skip this row if No Job is checked
+
+        # Common fields
+        base_fields = {
+            "machine": row.machine_name,
+            "date": doc.date,
+            "shift": doc.shift,
+            "operator": row.operator_name,
+        }
+
+        # Job 1
+        if row.job_one_name and row.job_one_number and row.job_one_quantity:
+            job_card_1 = frappe.new_doc("Job Card")
+            job_card_1.update(base_fields)
+            job_card_1.update({
+                "job_name": row.job_one_name,
+                "job_number": row.job_one_number,
+                "target_quantity": row.job_one_quantity,
+                "job_sequence_number": 1,
+            })
+            job_card_1.save()
+            created_count += 1
+
+        # Job 2
+        if row.job_two_name and row.job_two_number and row.job_two_quantity:
+            job_card_2 = frappe.new_doc("Job Card")
+            job_card_2.update(base_fields)
+            job_card_2.update({
+                "job_name": row.job_two_name,
+                "job_number": row.job_two_number,
+                "target_quantity": row.job_two_quantity,
+                "job_sequence_number": 2,
+            })
+            job_card_2.save()
+            created_count += 1
+
+    # Optionally update Job Plan status
+    doc.status = "Confirmed"
+    doc.save()
+    return {"status": "success", "created": created_count}
